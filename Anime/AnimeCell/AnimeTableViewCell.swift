d4ed2722ac8b349a982a4b7771fd0829163e6b14
Lee Mccarthy-Pirie <lee.mccarthy.pirie@verizonconnect.com>
2021-04-27 15:10:36 +0100

    Update

---
+++ Anime/AnimeCell/AnimeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AnimeTableViewCell.swift
+//  Anime
+//
+//  Created by Lee Mccarthy-Pirie on 22/04/2021.
+//
+
+import UIKit
+
+class AnimeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
